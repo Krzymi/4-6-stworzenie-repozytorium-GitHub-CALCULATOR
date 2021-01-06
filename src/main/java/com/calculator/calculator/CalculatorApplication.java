b30b8095ff406383993f8b68d0c07b4929faaea2
Sylwia Krzymowska <s.krzymowska@gmail.com>
2021-01-06 20:06:57 +0100

    update file

--- src/main/java/com/calculator/calculator/CalculatorApplication.java
+++ src/main/java/com/calculator/calculator/CalculatorApplication.java
@@ -3,11 +3,38 @@ package com.calculator.calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+class Calculator {
+    private int x;
+    private int y;
+
+    public Calculator(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void Adding() {
+        int myResult;
+        myResult = x + y;
+        System.out.println("Result of adding " + x + " to " + y + " is: " +myResult);
+    }
+
+    public void Substracting() {
+        int myResult;
+        myResult = x - y;
+        System.out.println("Result of substracting " + y + " from " + x + " is: " +myResult);
+    }
+}
+
 @SpringBootApplication
 public class CalculatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CalculatorApplication.class, args);
-    }
 
+        SpringApplication.run(CalculatorApplication.class, args);
+        Calculator calculator1 = new Calculator(2, 5);
+        calculator1.Adding();
+
+        Calculator calculator2 = new Calculator(7, 5);
+        calculator2.Substracting();
+    }
 }
